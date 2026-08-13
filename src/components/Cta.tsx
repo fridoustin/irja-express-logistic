bad37c461d7ad19027c26a8f0ee1d82e503eed9f
fridoustin <fridolinaustin16@gmail.com>
2026-08-13 13:23:45 +0800

    feat: redirect "isi formulir kontak" to /contact

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -22,7 +22,7 @@ export default function Cta() {
               <path d="M5 12h14M13 6l6 6-6 6" />
             </svg>
           </a>
-          <a href="#contact" className="btn btn-outline">
+          <a href="/contact" className="btn btn-outline">
             Isi Formulir Kontak
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M5 12h14M13 6l6 6-6 6" />
